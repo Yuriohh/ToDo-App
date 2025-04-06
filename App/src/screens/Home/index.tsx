52416677b11e7849b8ac1777313ace321ee29cb8
yuri65 <yuri@redeparcerias.com>
2025-04-06 13:10:39 -0300

    feat: add Home component

---
+++ App/src/screens/Home/index.tsx
@@ -0,0 +1,69 @@
+import { FlatList, Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
+import { Rocket } from "../../components/Rocket";
+import { To } from "../../components/To";
+import { Do } from "../../components/Do";
+import { Tasks } from "../../components/Tasks";
+import { useState } from "react";
+import { Clipboard } from "../../components/Clipboard";
+
+export function Home() {
+    const [tasks, setTasks] = useState<string[]>([]);
+    const [textTask, setTextTask] = useState('');
+
+    function addTask() {
+        setTasks((prevState) => [...prevState, textTask]);
+        setTextTask('');
+    }
+
+    function handleRemove(item: string) {
+
+    }
+
+    return (
+        <>
+            <View style={styles.header}>
+                <View style={styles.logoTitle}>
+                    <Rocket style={{ marginRight: 14 }} />
+                    <To />
+                    <Do />
+                </View>
+
+                <View style={styles.form}>
+                    <TextInput style={styles.input} placeholder='Adicione uma nova tarefa' placeholderTextColor={'#808080'} />
+                    <TouchableOpacity style={styles.button} onPress={addTask}>
+                        <Image source={require('../../../../assets/icons/plus.png')} style={styles.textButton} />
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            <View style={styles.body}>
+                <View style={styles.info}>
+                    <Text style={styles.created}>Criadas</Text>
+                    <Text style={styles.done}>Concluidas</Text>
+                </View>
+
+
+                <FlatList
+                    data={tasks}
+                    keyExtractor={(item) => item}
+                    renderItem={({ item }) => (
+                        <Tasks
+                            key={item}
+                            name={item}
+                            onRemove={() => handleRemove(item)}
+                        />
+                    )}
+                    showsVerticalScrollIndicator={false}
+                    ListEmptyComponent={() => (
+                        <View style={styles.emptyList}>
+                            <Clipboard />
+                            <Text style={styles.emptyListText}>Você ainda não tem tarefas cadastradas</Text>
+                            <Text style={styles.emptyListText2}>Crie tarefas e organize seus itens a fazer</Text>
+                        </View>
+                    )}
+                />
+            </View>
+        </>
+    )
+}
